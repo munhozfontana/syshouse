82640373d2a95991f6c4f393f89b31b52ecbbd64
munhozfontana <munhozfontana@gmail.com>
2020-07-02 15:07:48 -0300

    add socio entity

---
+++ lib/app/domain/entities/socio.dart
@@ -0,0 +1,26 @@
+import 'package:equatable/equatable.dart';
+
+class Socio extends Equatable {
+  final String id;
+  final String createdAt;
+  final String nome;
+  final String cpf;
+  final String rg;
+  final String nacionalidade;
+  final String estadoCivil;
+  final String profissao;
+
+  Socio({
+    this.id,
+    this.createdAt,
+    this.nome,
+    this.cpf,
+    this.rg,
+    this.nacionalidade,
+    this.estadoCivil,
+    this.profissao,
+  });
+
+  @override
+  List<Object> get props => [id];
+}
